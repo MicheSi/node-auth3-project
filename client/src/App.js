377f332ea7cd0e3143819ac9c68e5894ccdf8ce8
mlazzell17@gmail.com <MicheSi>
2020-02-26 16:28:24 -0700

    created sign up and sign in forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+import PrivateRoute from './utils/PrivateRoute';
+import Signup from './components/signup';
 
 import './App.css';
+import SignIn from './components/signin';
+
 
 function App() {
   return (
     <div className="App">
+      <h1>Node Auth3 Project</h1>
+      <Switch>
+        <PrivateRoute exact path='/users' />
+        <Route exact path='/signin' component={SignIn} />
+        <Route exact path='/' component={Signup} />
+      </Switch>
       
     </div>
   );
---
+++ client/src/components/signup.js
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { Button, Form } from 'semantic-ui-react'
+
+const Signup = props => {
+
+    const initialState = {
+        username: '',
+        password: '',
+        department: ''
+    }
+
+    const [user, setUser] = useState(initialState);
+
+    const handleChange = e => {
+        console.log(e.target.name, e.target.value)
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+        console.log('new user', user)
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        setUser({...user})
+        axiosWithAuth()
+            .post('/auth/register', user)
+            .then(res => {
+                setUser(user)
+                window.location.href='/login'
+            })
+            .catch(err => console.log('cannot sign up user', err))
+    }
+
+    return (
+        <div className='signupForm'>
+            <h2>Sign Up</h2>
+            <Form onSubmit={submitForm}>
+                <Form.Group widths='equal'>
+                    <Form.Input
+                     fluid
+                     id='username'
+                     type='text'
+                     name='username'
+                     label='Username'
+                     placeholder='Enter a Username'
+                     onChange={handleChange}
+                     value={user.username}
+                    />
+                    <Form.Input
+                     fluid
+                     id='password'
+                     type='password'
+                     name='password'
+                     label='Password'
+                     placeholder='Enter a Password'
+                     onChange={handleChange}
+                     value={user.password}
+                    />
+                    <Form.Input
+                     fluid
+                     id='department'
+                     type='text'
+                     name='department'
+                     label='Department'
+                     placeholder='Enter Department'
+                     onChange={handleChange}
+                     value={user.department}
+                    />
+                </Form.Group>
+                <Button type='submit'>Sign Up</Button>
+            </Form>
+            
+
+        </div>
+    )
+}
+
+export default Signup;
