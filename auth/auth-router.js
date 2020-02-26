d9cf92a9e29fc5ceab32a00bd92da156ee60c174
mlazzell17@gmail.com <MicheSi>
2020-02-26 14:22:38 -0700

    register and log in working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,12 +2,56 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-router.post('/register', (req, res) => {
+const Users = require('../users/users-model');
+const {jwtSecret} = require('../config/secrets');
 
+router.post('/register', (req, res) => {
+    const user = req.body;
+    const hash = bcrypt.hashSync(user.password, 8);
+    user.password = hash;
+
+    Users.add(user)
+        .then(newUser => {
+            res.status(201).json(newUser);
+        })
+        .catch(err => {
+            res.status(500).json({message: err});
+        })
 })
 
 router.post('/login', (req, res) => {
-    
+    const {username, password} = req.body;
+
+    Users.findBy({username})
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+
+                res.status(200).json({
+                    message: `Welcome ${user.username}!`,
+                    token,
+                })
+            } else {
+                res.status(401).json({message: 'Invalid credentials'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({message: err});
+        })
 })
+
+function generateToken(user) {
+    const payload = {
+        username: user.username,
+        department: user.department
+    }
+
+    const options = {
+        expiresIn: '1d'
+    }
+
+    return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+const Users = require('./users-model');
 
+router.get('/', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({message: err})
+        })
 })
 
 module.exports = router;
