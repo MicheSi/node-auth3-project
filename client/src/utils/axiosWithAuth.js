5a97f408547005302b6926d5653fda2b7609cfec
mlazzell17@gmail.com <MicheSi>
2020-02-27 09:21:48 -0700

    changed server port because of error

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
 
     return axios.create({
-        baseURL: 'http://localhost:8000/api',
+        baseURL: 'http://localhost:5555/api',
         headers: {
             authorization: token
         }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5555;
 server.listen(port, () => console.log(`\n** Server is running on port ${port} **\n`));
