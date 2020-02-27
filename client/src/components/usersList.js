bb610a87cfea8988c089acd9b6beaae1a0300425
mlazzell17@gmail.com <MicheSi>
2020-02-26 17:07:02 -0700

    added sign out button

--- client/src/components/usersList.js
+++ client/src/components/usersList.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
 import UserCard from './userCard';
+import {Button} from 'semantic-ui-react';
 
 const UsersList = props => {
     const [users, setUsers] = useState([]);
@@ -15,8 +16,15 @@ const UsersList = props => {
         .catch(err => console.log('cannot retrieve users', err));
     }, [])
 
+    const signout = e => {
+        e.preventDefault()
+        localStorage.clear();
+        window.location.href='/signin'
+    }
+
     return (
         <div className='usersList'>
+            <Button className='signoutBtn' onClick={signout}>Sign Out</Button>
             {users.map(user => (
                 <UserCard
                  key={user.id}
